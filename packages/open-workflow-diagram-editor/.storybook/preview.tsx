f37361948b49eed02e1dc0bbe7c85414e19faceb
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2026-08-19 10:33:59 +0100

    Create Nested Editing Stories (#333)
    * feat: add nested editing sample workflows
    
    Signed-off-by: Cheryl Kong <cherylkong50@gmail.com>
    Signed-off-by: lornakelly <lornakelly88@gmail.com>

--- packages/open-workflow-diagram-editor/.storybook/preview.tsx
+++ packages/open-workflow-diagram-editor/.storybook/preview.tsx
@@ -44,7 +44,7 @@ const preview: Preview = {
 
     options: {
       storySort: {
-        order: ["Introduction", "Features", "Examples", "Use Cases"],
+        order: ["Introduction", "Features", "Examples", "Use Cases", "Nested Editing"],
       },
     },
   },
